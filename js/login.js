a8dd2a251a1238e300185b255efd93be8f19de31
chiangmengting <53255886+chiangmengting@users.noreply.github.com>
2020-06-02 20:49:14 +0800

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,17 @@
+const toggleLogin = function()  {
+    document.getElementById("login-toggle").style.backgroundColor = "#019BD9";
+    document.getElementById("login-toggle").style.color = "#fff";
+    document.getElementById("signup-toggle").style.backgroundColor = "#fff";
+    document.getElementById("signup-toggle").style.color = "#222";
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+}
+
+const toggleSignup = function() {
+    document.getElementById("login-toggle").style.backgroundColor = "#fff";
+    document.getElementById("login-toggle").style.color = "#222";
+    document.getElementById("signup-toggle").style.backgroundColor = "#019BD9";
+    document.getElementById("signup-toggle").style.color = "#fff";
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+}
